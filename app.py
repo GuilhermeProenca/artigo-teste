0165966d6b374387fd21cc4827fdc91086a19c4f
GuilhermeProenca <34319735+GuilhermeProenca@users.noreply.github.com>
2023-12-29 21:29:30 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -9,7 +9,8 @@ from PIL import Image
 st.title("Projeto Detecção de Medidas Corporais Através de Imagens Para Cálculos de Avaliação Física")
 st.text("Objetivo: Dada informações de altura, peso, idade, gênero e imagem de frente/lado,\nfornecer informações sobre uma pessoa.")
 
-gender = st.selectbox("Gênero:", ("Feminino", "Masculino"), placeholder="Selecione o gênero")
+st.markdown("### Selecione uma opção:")
+gender = st.selectbox("Gênero:", ["Feminino", "Masculino"])
 
 if (gender == "Feminino"):
     gender = "F"
