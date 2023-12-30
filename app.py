973abd7c8d40e4f3a04ce216544bf383304572d9
GuilhermeProenca <34319735+GuilhermeProenca@users.noreply.github.com>
2023-12-30 17:31:31 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from PIL import Image
 
 idioma_selecionado = ""
 
-if idioma_selecionado
+if (idioma_selecionado != "Inglês"):
     idioma_selecionado = st.radio("Selecione o idioma", ["Português", "Inglês"], index=0, key="pt-br")
 else:
     idioma_selecionado = st.radio("Select the language", ["Portuguese", "English"], index=0, key="en-us")
