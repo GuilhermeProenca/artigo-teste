cd406d2581fe0fc6c13035f340c963401bfb5538
GuilhermeProenca <34319735+GuilhermeProenca@users.noreply.github.com>
2024-01-01 20:18:09 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -6,115 +6,131 @@ import joblib
 
 from PIL import Image
 
-idioma_selecionado = st.radio("Selecione o idioma - Select the language", ["Português - Portuguese", "Inglês - English"], index=0, key="language")
+selected_language = st.radio("Selecione o idioma - Select the language", ["Português - Portuguese", "Inglês - English"], index=0, key="language")
 
 # Dicionário para mapear os textos em português e inglês
-textos = {
+texts = {
     "Português - Portuguese": {
-        "titulo": "Projeto Detecção de Medidas Corporais Através de Imagens Para Cálculos de Avaliação Física",
-        "objetivo": "Objetivo: Com os dados de altura, peso, idade, gênero e imagem de frente/lado,\nfornecer informações sobre uma pessoa.",
-        "foto de frente": "Foto de frente",
-        "foto de lado": "Foto de lado",
-        "carregar": "Carregue a imagem",
-        "genero": "Gênero",
-        "seleciona genero": "Selecione o gênero",
-        "menu genero": ["Feminino", "Masculino"],
-        "idade": "Idade",
-        "exemplo idade": "Exemplo: 35 anos",
-        "altura": "Altura",
-        "exemplo altura": "Exemplo: 170 cm",
-        "peso": "Peso",
-        "exemplo peso": "Exemplo: 80 kg",
-        "fator": "Fator de atividade",
-        "seleciona fator": "Selecione uma opção",
-        "menu fator": ["Sedentário - Pouco ou nenhum exercício", \
-                      "Levemente ativo - Exercício leve de 1 a 3 dias por semana",\
-                      "Moderadamente ativo - Pratica esportes de 3 a 5 dias por semana",\
-                      "Muito ativo - Exercícios intensos de 5 a 6 dias por semana",\
-                      "Extremamente ativo - Exercícios intensos diariamente ou até 2 vezes por dia"],
-        "status gcm": ["Magreza",\
+        "title": "Projeto Detecção de Medidas Corporais Através de Imagens Para Cálculos de Avaliação Física",
+        "objective": "Objetivo: Com os dados de altura, peso, idade, gênero e imagem de frente/lado,\nfornecer informações sobre uma pessoa.",
+        "front_photo": "Foto de frente",
+        "side_photo": "Foto de lado",
+        "load": "Carregue a imagem",
+        "gender": "Gênero",
+        "gender_select": "Selecione o gênero",
+        "gender_menu": ["Feminino", "Masculino"],
+        "age": "Idade",
+        "age_example": "Exemplo: 35 anos",
+        "height": "Altura",
+        "height_example": "Exemplo: 170 cm",
+        "weight": "Peso,
+        "weight_example": "Exemplo: 80 kg",
+        "factor": "Fator de atividade",
+        "factor_select": "Selecione uma opção",
+        "factor_menu": ["Sedentário - Pouco ou nenhum exercício",\
+                        "Levemente ativo - Exercício leve de 1 a 3 dias por semana",\
+                        "Moderadamente ativo - Pratica esportes de 3 a 5 dias por semana",\
+                        "Muito ativo - Exercícios intensos de 5 a 6 dias por semana",\
+                        "Extremamente ativo - Exercícios intensos diariamente ou até 2 vezes por dia"],
+        "status_gcm": ["Magreza",\
                        "Normal",\
                        "Sobrepeso",\
                        "Obesidade"]
+        "status_imc": ["Muito abaixo do peso",\
+                       "Abaixo do peso",\
+                       "Peso normal",\
+                       "Acima do peso",\
+                       "Obesidade I",\
+                       "Obesidade II (severa)",\
+                       "Obesidade III (mórbida)"],
+        "result_button": "Resultado"
     },
     "Inglês - English": {
-        "titulo": "Body Measurement Detection Project Through Images for Physical Assessment Calculations",
-        "objetivo": "Objective: With height, weight, age, gender, and front/side image data,\nprovide information about a person.",
-        "foto de frente": "Front photo",
-        "foto de lado": "Side photo",
-        "carregar": "Upload the image",
-        "genero": "Gender",
-        "seleciona genero": "Select the genre",
-        "menu genero": ["Female", "Male"],
-        "idade": "Age",
-        "exemplo idade": "Example: 35 years",
-        "altura": "Height",
-        "exemplo altura": "Example: 170 cm",
-        "peso": "Weight",
-        "exemplo peso": "Example: 80 kg",
-        "fator": "Activity factor",
-        "seleciona fator": "Select an option",
-        "menu fator": ["Sedentary - Little or no exercise", \
-                       "Lightly Active - Light exercise 1 to 3 days a week",\
-                       "Moderately active - Plays sports 3 to 5 days a week",\
-                       "Very active - Intense exercise 5 to 6 days a week",\
-                       "Extremely active - Intense exercise daily or up to twice a day"],
-        "status gcm": ["Thinness",\
+        "title": "Body Measurement Detection Project Through Images for Physical Assessment Calculations",
+        "objective": "Objective: With height, weight, age, gender, and front/side image data,\nprovide information about a person.",
+        "front_photo": "Front photo",
+        "side_photo": "Side photo",
+        "load": "Upload the image",
+        "gender": "Gender",
+        "gender_select": "Select the genre",
+        "gender_menu": ["Female", "Male"],
+        "age": "Age",
+        "age_example": "Example: 35 years",
+        "height": "Height",
+        "height_example": "Example: 170 cm",
+        "weight": "Weight",
+        "weight_example": "Example: 80 kg",
+        "factor": "Activity factor",
+        "factor_select": "Select an option",
+        "factor_menu": ["Sedentary - Little or no exercise", \
+                        "Lightly Active - Light exercise 1 to 3 days a week",\
+                        "Moderately active - Plays sports 3 to 5 days a week",\
+                        "Very active - Intense exercise 5 to 6 days a week",\
+                        "Extremely active - Intense exercise daily or up to twice a day"],
+        "status_gcm": ["Thinness",\
                        "Normal",\
                        "Overweight",\
                        "Obesity"]
+        "status_imc": ["Very underweight",\
+                       "Under weight",\
+                       "Normal weight",\
+                       "Overweight",\
+                       "Obesity I",\
+                       "Obesity II (severe)",\
+                       "Obesity III (morbid)"],
+        "result_button": "Result"
     }
 }
 
 # Atualiza os textos com base na seleção de idioma
-titulo = textos[idioma_selecionado]["titulo"]
-objetivo = textos[idioma_selecionado]["objetivo"]
-foto_de_frente = textos[idioma_selecionado]["foto de frente"]
-foto_de_lado = textos[idioma_selecionado]["foto de lado"]
-carregar = textos[idioma_selecionado]["carregar"]
-genero = textos[idioma_selecionado]["genero"]
-seleciona_genero = textos[idioma_selecionado]["seleciona genero"]
-menu_genero = textos[idioma_selecionado]["menu genero"]
-idade = textos[idioma_selecionado]["idade"]
-exemplo_idade = textos[idioma_selecionado]["exemplo idade"]
-altura = textos[idioma_selecionado]["altura"]
-exemplo_altura = textos[idioma_selecionado]["exemplo altura"]
-peso = textos[idioma_selecionado]["peso"]
-exemplo_peso = textos[idioma_selecionado]["exemplo peso"]
-fator = textos[idioma_selecionado]["fator"]
-seleciona_fator = textos[idioma_selecionado]["seleciona fator"]
-menu_fator = textos[idioma_selecionado]["menu fator"]
+title = texts[selected_language]["title"]
+objective = texts[selected_language]["objective"]
+front_photo = texts[selected_language]["front_photo"]
+side_photo = texts[selected_language]["side_photo"]
+load = texts[selected_language]["load"]
+gender_text = texts[selected_language]["gender"]
+gender_select = texts[selected_language]["gender_select"]
+gender_menu = texts[selected_language]["gender_menu"]
+age_text = texts[selected_language]["age"]
+age_example = texts[selected_language]["age_example"]
+height_text = texts[selected_language]["height"]
+height_example = texts[selected_language]["height_example"]
+weight_text = texts[selected_language]["weight"]
+weight_example = texts[selected_language]["weight_example"]
+factor_text = texts[selected_language]["factor"]
+factor_select = texts[selected_language]["factor_select"]
+factor_menu = texts[selected_language]["factor_menu"]
 
-# Exibição dos textos no Streamlit
-st.title(titulo)
-st.text(objetivo)
+# Exibição dos textos iniciais no Streamlit
+st.title(title)
+st.text(objective)
 
 st.write("---")
 
-st.markdown(f"### {foto_de_frente}")
-uploaded_file1 = st.file_uploader(carregar, key="file1")
+st.markdown(f"### {front_photo}")
+uploaded_file1 = st.file_uploader(load, key="file1")
 
-st.markdown(f"### {foto_de_lado}")
-uploaded_file2 = st.file_uploader(carregar, key="file2")
+st.markdown(f"### {side_photo}")
+uploaded_file2 = st.file_uploader(load, key="file2")
 
-st.markdown(f"### {genero}")
-gender = st.selectbox(seleciona_genero, menu_genero)
+st.markdown(f"### {gender_text}")
+gender = st.selectbox(gender_select, gender_menu)
 
-st.markdown(f"### {idade}")
-age = st.number_input(exemplo_idade, format="%i", min_value=18)
+st.markdown(f"### {age_text}")
+age = st.number_input(age_example, format="%i", min_value=18)
 
-st.markdown(f"### {altura}")
-measure_height = st.number_input(exemplo_altura, format="%.2f", min_value=100.0, step=0.10)
+st.markdown(f"### {height_text}")
+measure_height = st.number_input(height_example, format="%.2f", min_value=100.0, step=0.10)
 
-st.markdown(f"### {peso}")
-weight = st.number_input(exemplo_peso, format="%.2f", min_value=40.0, step=0.10)
+st.markdown(f"### {weight_text}")
+weight = st.number_input(weight_example, format="%.2f", min_value=40.0, step=0.10)
 
-st.markdown(f"### {fator}")
-activity_factor = st.selectbox(seleciona_fator, menu_fator, index=0, key="factor")
+st.markdown(f"### {factor_text}")
+activity_factor = st.selectbox(factor_select, factor_menu, index=0, key="factor")
 
-neck_measure = 38.0
+measure_neck = 38.0
 measure_waist = 75.0
-hip_measure = 90.0
+measure_hip = 90.0
 
 
 # Coversão de variáveis
@@ -125,13 +141,13 @@ else:
 
 
 # Funções dos cálculos
-def calc_bioimpedance(gender, measure_height, neck_measure, measure_waist, hip_measure):
+def calc_bioimpedance(gender, measure_height, measure_neck, measure_waist, measure_hip):
 
     if (gender.upper() == "F"):
-        gcm = (495 / (1.296 - 0.350 * math.log10(hip_measure + measure_waist - neck_measure) + 0.221 * \
+        gcm = (495 / (1.296 - 0.350 * math.log10(measure_hip + measure_waist - measure_neck) + 0.221 * \
                       math.log10(measure_height))) - 450
     else:
-        gcm = (495 / (1.033 - 0.191 * math.log10(measure_waist - neck_measure) + 0.155 * \
+        gcm = (495 / (1.033 - 0.191 * math.log10(measure_waist - measure_neck) + 0.155 * \
                       math.log10(measure_height))) - 450
     
     return gcm
@@ -207,42 +223,42 @@ def status_gcm(result_gcm, gender, age):
            (age <= 39 and result_gcm <= 20.0) or \
            (age <= 59 and result_gcm <= 22.0) or \
            (age >= 60 and result_gcm <= 23.0)):
-            status_gcm = textos[idioma_selecionado]["status gcm"][0]
+            status_gcm = texts[selected_language]["status_gcm"][0]
         elif((age == 18 and result_gcm <= 30.0) or \
             (age == 19 and result_gcm <= 31.0) or \
             (age <= 39 and result_gcm <= 32.0) or \
             (age <= 59 and result_gcm <= 33.0) or \
             (age >= 60 and result_gcm <= 35.0)):
-            status_gcm = textos[idioma_selecionado]["status gcm"][1]
+            status_gcm = texts[selected_language]["status_gcm"][1]
         elif((age == 18 and result_gcm <= 35.0) or \
             (age == 19 and result_gcm <= 36.0) or \
             (age <= 39 and result_gcm <= 38.0) or \
             (age <= 59 and result_gcm <= 39.0) or \
             (age >= 60 and result_gcm <= 41.0)):
-            status_gcm = textos[idioma_selecionado]["status gcm"][2]
+            status_gcm = texts[selected_language]["status_gcm"][2]
         else:
-            status_gcm = textos[idioma_selecionado]["status gcm"][3]
+            status_gcm = texts[selected_language]["status_gcm"][3]
     else:
         if((age == 18 and result_gcm <= 9.0) or \
            (age == 19 and result_gcm <= 8.0) or \
            (age <= 39 and result_gcm <= 7.0) or \
            (age <= 59 and result_gcm <= 10.0) or \
            (age >= 60 and result_gcm <= 12.0)):
-            status_gcm = textos[idioma_selecionado]["status gcm"][0]
+            status_gcm = texts[selected_language]["status_gcm"][0]
         elif((age == 18 and result_gcm <= 19.0) or \
             (age == 19 and result_gcm <= 19.0) or \
             (age <= 39 and result_gcm <= 19.0) or \
             (age <= 59 and result_gcm <= 21.0) or \
             (age >= 60 and result_gcm <= 24.0)):
-            status_gcm = textos[idioma_selecionado]["status gcm"][1]
+            status_gcm = texts[selected_language]["status_gcm"][1]
         elif((age == 18 and result_gcm <= 23.0) or \
             (age == 19 and result_gcm <= 23.0) or \
             (age <= 39 and result_gcm <= 24.0) or \
             (age <= 59 and result_gcm <= 27.0) or \
             (age >= 60 and result_gcm <= 29.0)):
-            status_gcm = textos[idioma_selecionado]["status gcm"][2]
+            status_gcm = texts[selected_language]["status_gcm"][2]
         else:
-            status_gcm = textos[idioma_selecionado]["status gcm"][3]
+            status_gcm = texts[selected_language]["status_gcm"][3]
             
     return status_gcm
 
@@ -250,24 +266,24 @@ def status_gcm(result_gcm, gender, age):
 def status_imc(result_imc):
     
     if(result_imc < 17):
-        status_imc = "Muito abaixo do peso"
+        status_imc = texts[selected_language]["status_imc"][0]
     elif(result_imc <= 18.49):
-        status_imc = "Abaixo do peso"
+        status_imc = texts[selected_language]["status_imc"][1]
     elif(result_imc <= 24.99):
-        status_imc = "Peso normal"
+        status_imc = texts[selected_language]["status_imc"][2]
     elif(result_imc <= 29.99):
-        status_imc = "Acima do peso"
+        status_imc = texts[selected_language]["status_imc"][3]
     elif(result_imc <= 34.99):
-        status_imc = "Obesidade I"
+        status_imc = texts[selected_language]["status_imc"][4]
     elif(result_imc <= 39.99):
-        status_imc = "Obesidade II (severa)"
+        status_imc = texts[selected_language]["status_imc"][5]
     else:
-        status_imc = "Obesidade III (mórbida)"
+        status_imc = texts[selected_language]["status_imc"][6]
         
     return status_imc
 
 
-result_gcm = calc_bioimpedance(gender, measure_height, neck_measure, measure_waist, hip_measure)
+result_gcm = calc_bioimpedance(gender, measure_height, measure_neck, measure_waist, measure_hip)
 
 result_imc = calc_imc(weight, measure_height)
 
@@ -283,7 +299,7 @@ gcm_status = status_gcm(result_gcm, gender, age)
 
 imc_status = status_imc(result_imc)
 
-if st.button("Resultado"):
+if st.button(texts[selected_language]["result_button"]):
     st.write("---")
 
     with st.sidebar:
